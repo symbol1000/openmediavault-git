19c83962883d6a850f4f1803ce57b74482b68949
Aaron Murray <plugins@omv-extras.org>
2014-04-14 10:59:01 -0500

    update copyrights

--- var/www/openmediavault/js/omv/module/admin/service/git/Info.js
+++ var/www/openmediavault/js/omv/module/admin/service/git/Info.js
@@ -1,22 +1,21 @@
 /**
- *
  * @license    http://www.gnu.org/licenses/gpl.html GPL Version 3
  * @author     OpenMediaVault Plugin Developers <plugins@omv-extras.org>
- * @copyright  Copyright (c) 2013 OpenMediaVault Plugin Developers 
+ * @copyright  Copyright (c) 2009-2013 Volker Theile
+ * @copyright  Copyright (c) 2013-2014 OpenMediaVault Plugin Developers
  *
- * This file is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * This file is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this file. If not, see <http://www.gnu.org/licenses/>.
- *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 // require("js/omv/WorkspaceManager.js")
 // require("js/omv/workspace/form/Panel.js")
@@ -39,7 +38,7 @@ autoLoadData    : false,
         return [{
             /* Git info */
             xtype : "fieldset",
-	     title: _("Git"),
+         title: _("Git"),
             layout : "fit",
             items : [{
                 border  : false,
